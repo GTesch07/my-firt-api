64ba2b38c2ee4bb23ebba8b93892f969d886ce79
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-07-23 16:12:44 -0300

    test: add new Mapping for delete users by Id.

--- src/main/java/dio/web/api/controller/UserController.java
+++ src/main/java/dio/web/api/controller/UserController.java
@@ -3,6 +3,7 @@ package dio.web.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,10 @@ public class UserController {
     @GetMapping("/users/{username}")
     public User getOne(@PathVariable("username") String username) {
         return repository.findByUsername(username);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void remove(@PathVariable("id") Integer id) {
+        repository.remove(id);
     }
 }
